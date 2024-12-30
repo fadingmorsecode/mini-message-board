db25853010e9047a754a64beb237ac2b7d66c1a6
Drake Williamson <106215095+fadingmorsecode@users.noreply.github.com>
2024-12-30 13:39:37 -0800

    create public folder to serve static assets

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const path = require('node:path');
 const { indexRouter } = require('./routers/indexRouter');
 const messageRouter = require('./routers/messageRouter');
 const app = express();
+const assetsPath = path.join(__dirname, 'public');
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 app.use('/new', messageRouter);
+app.use(express.static(assetsPath));
 
 const PORT = 999;
 app.listen(PORT);
