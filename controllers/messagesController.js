08a01db601514e4cb2cc186c25f1b7569e302e69
Drake Williamson <106215095+fadingmorsecode@users.noreply.github.com>
2024-12-30 00:14:55 -0800

    remove console logs

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -2,13 +2,7 @@ const db = require('../db');
 
 async function getMessage(req, res) {
   const { messageId } = req.params;
-  console.log(messageId);
-  console.log(db.getMessages());
   const message = await db.getMessageById(messageId);
-  console.log('the message in question');
-  console.log(message);
-  console.log(await db.getMessages());
-  console.log(message);
   res.render('details', { message: message });
 }
 
