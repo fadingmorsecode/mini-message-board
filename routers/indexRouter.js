5b5f5bc18bddf2723f22f858b9efbc28d06f2a9c
Drake Williamson <106215095+fadingmorsecode@users.noreply.github.com>
2025-01-05 08:09:10 -0800

    remove unnecessary redirect

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -16,10 +16,6 @@ indexRouter.get('/', async (req, res) =>
 
 indexRouter.get('/message/:messageId', getMessage);
 
-indexRouter.get('/github', (req, res) => {
-  res.redirect('https://github.com/fadingmorsecode');
-});
-
 indexRouter.post('/new', async (req, res) => {
   const newMessage = req.body;
   await db.createMessage({
